987c92816e1a33227030939a7ce4b1fee4ff4c1e
GitHub Action <action@github.com>
2024-05-14 17:49:46 +0000

    Initialize template repository proper
    Set package name to com.github.kargone.skyblockflips2
    Set project name to Skyblock-Flips2
    Set modid to skyblockflips

--- src/main/java/com/github/kargone/skyblockflips2/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/kargone/skyblockflips2/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.kargone.skyblockflips2.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
